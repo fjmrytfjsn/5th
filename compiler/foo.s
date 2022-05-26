729bf9e2c7608610a3efc3f4ceaa8d77a06a9789
fjmrytfjsn <e1832@s.akashi.ac.jp>
2022-05-26 12:14:57 +0900

    Add compiler tasks
    コンパイラの課題を追加しました。

--- compiler/foo.s
+++ compiler/foo.s
@@ -1,24 +1,43 @@
 	.file	"foo.c"
 	.text
 	.globl	foo
-	.def	foo;	.scl	2;	.type	32;	.endef
-	.seh_proc	foo
+	.type	foo, @function
 foo:
+.LFB0:
+	.cfi_startproc
+	endbr64
 	pushq	%rbp
-	.seh_pushreg	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$16, %rsp
-	.seh_stackalloc	16
-	.seh_endprologue
-	movl	%ecx, 16(%rbp)
-	movl	16(%rbp), %eax
-	imull	16(%rbp), %eax
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	-20(%rbp), %eax
+	imull	%eax, %eax
 	movl	%eax, -4(%rbp)
 	movl	-4(%rbp), %eax
 	addl	$2, %eax
-	addq	$16, %rsp
 	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
-	.seh_endproc
-	.ident	"GCC: (Rev1, Built by MSYS2 project) 8.3.0"
+	.cfi_endproc
+.LFE0:
+	.size	foo, .-foo
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
