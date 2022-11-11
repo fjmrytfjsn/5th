62b559975aba740b1a0f226bba07723a6053062b
fjmrytfjsn <e1832@s.akashi.ac.jp>
2022-11-11 12:15:21 +0900

    U

--- img-ENG/task1/simple_mix.m
+++ img-ENG/task1/simple_mix.m
@@ -7,10 +7,6 @@ function simple_mix(file_name_1, file_name_2, ratio, another_name)
 img_1 = imread(file_name_1);
 img_2 = imread(file_name_2);
 
-converted_img(:,:,[1])=ratio*img_1(:,:,[1])+(1-ratio)*img_2(:,:,[1]);
-converted_img(:,:,[2])=ratio*img_1(:,:,[2])+(1-ratio)*img_2(:,:,[2]);
-converted_img(:,:,[3])=ratio*img_1(:,:,[3])+(1-ratio)*img_2(:,:,[3]);
-
 imwrite(converted_img, another_name);
 
 endfunction
