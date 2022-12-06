4cae08edea3ca6e5d07318974c27ad65bf62a15c
fjsn <e1832@s.akashi.ac.jp>
2022-12-06 19:23:05 +0900

    Update construct.cpp
    CR(23, 4, 6)まで成功。

--- graduation-research/prog/my/construct.cpp
+++ graduation-research/prog/my/construct.cpp
@@ -54,17 +54,33 @@ Graph LOOP(int base, int n, int d1, int d2, Graph t) {
     return t;
 }
 
+Graph STEP4(int range, int n, int d1, int d2, Graph t) {
+    for(int i=1; i<range; i++) {
+        if(i==1 or i==d1 or i==d2) continue;
+        t.add_edge(i, (i+d1)%n);
+        t.add_edge(i, (i+d2)%n);
+        t.add_edge(i, (i+n-d1)%n);
+        t.add_edge(i, (i+n-d2)%n);
+        t = LOOP(i, n, d1, d2, t);
+    }
+
+    return t;
+}
+
 Graph STEP5(int n, int d1, int d2, Graph t) {
     //STEP5-1
     for(int i=1; i<n-d1; i++)
         if(i!=d1 and i!=d2 and i!=n-d1 and i!=n-d2) {
-            t.add_edge(i, i+d1);
+            if(i%d1!=0 or i%d2!=0) {
+                t.add_edge(i, i+d1);
+            }
         }
 
     //STEP5-2
     for(int i=1; i<n-d2; i++) {
-        if(i!=d1 and i!=d2 and i!=n-d1 and i!=n-d2)
+        if(i!=d1 and i!=d2 and i!=n-d1 and i!=n-d2){
             t.add_edge(i, i+d2);
+        }
     }
 
     //STEP5-3
@@ -119,36 +135,38 @@ Graph construct0(int n, int d1, int d2) {
     for(int i=1+1; i<d1; i++)
         t0.add_edge(i, i+1);
     
-    //STEP4
-    for(int i=2; i<d1; i++) {
-        t0.add_edge(i, i+d1);
-        t0.add_edge(i, i+d2);
-        t0.add_edge(i, (i+n-d1)%n);
-        t0.add_edge(i, (i+n-d2)%n);
-        // now = i+d2;
-        // while(1) {
-        //     if(now!=d1 and now!=d2 and now!=n-d1 and now!=n-d2) {
-        //         if(now+d1<n-d1) {
-        //             t0.add_edge(now, now+d1);
-        //         }else {
-        //             ;
-        //         }
-        //         if(now+d2<n-d1) {
-        //             if(t0.add_edge(now, now+d2)) {
-        //                 ;
-        //             }else {
-        //                 break;
-        //             }
-        //         }else {
-        //             break;
-        //         }
-        //     }else {
-        //         break;
-        //     }
-        //     now += d2;
-        // }
-        t0 = LOOP(i, n, d1, d2, t0);
-    }
+    // //STEP4
+    // for(int i=2; i<n; i++) {
+    //     if(i==d1 or i==d2) continue;
+    //     t0.add_edge(i, (i+d1)%n);
+    //     t0.add_edge(i, (i+d2)%n);
+    //     t0.add_edge(i, (i+n-d1)%n);
+    //     t0.add_edge(i, (i+n-d2)%n);
+    //     // now = i+d2;
+    //     // while(1) {
+    //     //     if(now!=d1 and now!=d2 and now!=n-d1 and now!=n-d2) {
+    //     //         if(now+d1<n-d1) {
+    //     //             t0.add_edge(now, now+d1);
+    //     //         }else {
+    //     //             ;
+    //     //         }
+    //     //         if(now+d2<n-d1) {
+    //     //             if(t0.add_edge(now, now+d2)) {
+    //     //                 ;
+    //     //             }else {
+    //     //                 break;
+    //     //             }
+    //     //         }else {
+    //     //             break;
+    //     //         }
+    //     //     }else {
+    //     //         break;
+    //     //     }
+    //     //     now += d2;
+    //     // }
+    //     t0 = LOOP(i, n, d1, d2, t0);
+    // }
+    t0 = STEP4(d1, n, d1, d2, t0);
 
     // //STEP5-1
     // for(int i=1; i<n-d1; i++) {
@@ -218,42 +236,43 @@ Graph construct1(int n, int d1, int d2) {
     for(int i=d1+1; i<d2; i++)
         t1.add_edge(i, i+1);
     
-    //STEP4
-    for(int i=2; i<d2; i++) {
-        if(i==d1) continue;
-        t1.add_edge(i, i+d1);
-        t1.add_edge(i, (i+n-d1)%n);
-        t1.add_edge(i, (i+n-d2)%n);
-        if(t1.add_edge(i, i+d2)) {
-            ;
-        }else {
-            continue;
-        }
-        // now = i+d2;
-        // while(1) {
-        //     if(now!=d1 and now!=d2 and now!=n-d1 and now!=n-d2) {
-        //         if(now+d1<n-d1) {
-        //             t1.add_edge(now, now+d1);
-        //         }else {
-        //             ;
-        //         }
-        //         if(now+d2<n-d1) {
-        //             if(t1.add_edge(now, now+d2)) {
-        //                 ;
-        //             }else {
-        //                 break;
-        //             }
-        //         }else {
-        //             break;
-        //         }
-        //     }else {
-        //         break;
-        //     }
-        //     now += d2;
-        // }
+    // //STEP4
+    // for(int i=2; i<n; i++) {
+    //     if(i==d1 or i==d2) continue;
+    //     t1.add_edge(i, i+d1);
+    //     t1.add_edge(i, (i+n-d1)%n);
+    //     t1.add_edge(i, (i+n-d2)%n);
+    //     if(t1.add_edge(i, i+d2)) {
+    //         ;
+    //     }else {
+    //         continue;
+    //     }
+    //     // now = i+d2;
+    //     // while(1) {
+    //     //     if(now!=d1 and now!=d2 and now!=n-d1 and now!=n-d2) {
+    //     //         if(now+d1<n-d1) {
+    //     //             t1.add_edge(now, now+d1);
+    //     //         }else {
+    //     //             ;
+    //     //         }
+    //     //         if(now+d2<n-d1) {
+    //     //             if(t1.add_edge(now, now+d2)) {
+    //     //                 ;
+    //     //             }else {
+    //     //                 break;
+    //     //             }
+    //     //         }else {
+    //     //             break;
+    //     //         }
+    //     //     }else {
+    //     //         break;
+    //     //     }
+    //     //     now += d2;
+    //     // }
 
-        t1 = LOOP(i, n, d1, d2, t1);
-    }
+    //     t1 = LOOP(i, n, d1, d2, t1);
+    // }
+    t1 = STEP4(d2, n, d1, d2, t1);
 
     // //STEP5-1
     // for(int i=1; i<n-d1; i++) {
@@ -325,38 +344,39 @@ Graph construct2(int n, int d1, int d2) {
     for(int i=d2+1; i<n-d2; i++)
         t2.add_edge(i, i+1);
     
-    //STEP4
-    for(int i=2; i<n-d2; i++) {
-        if(i==d1 or i==d2) continue;
-        t2.add_edge(i, i+d1);
-        t2.add_edge(i, i+d2);
-        t2.add_edge(i, (i+n-d1)%n);
-        t2.add_edge(i, (i+n-d2)%n);
-        // now = i+d2;
-        // while(1) {
-        //     if(now!=d1 and now!=d2 and now!=n-d1 and now!=n-d2) {
-        //         if(now+d1<n-d1) {
-        //             t2.add_edge(now, now+d1);
-        //         }else {
-        //             ;
-        //         }
-        //         if(now+d2<n-d1) {
-        //             if(t2.add_edge(now, now+d2)) {
-        //                 ;
-        //             }else {
-        //                 break;
-        //             }
-        //         }else {
-        //             break;
-        //         }
-        //     }else {
-        //         break;
-        //     }
-        //     now += d2;
-        // }
+    // //STEP4
+    // for(int i=2; i<n; i++) {
+    //     if(i==d1 or i==d2) continue;
+    //     t2.add_edge(i, i+d1);
+    //     t2.add_edge(i, i+d2);
+    //     t2.add_edge(i, (i+n-d1)%n);
+    //     t2.add_edge(i, (i+n-d2)%n);
+    //     // now = i+d2;
+    //     // while(1) {
+    //     //     if(now!=d1 and now!=d2 and now!=n-d1 and now!=n-d2) {
+    //     //         if(now+d1<n-d1) {
+    //     //             t2.add_edge(now, now+d1);
+    //     //         }else {
+    //     //             ;
+    //     //         }
+    //     //         if(now+d2<n-d1) {
+    //     //             if(t2.add_edge(now, now+d2)) {
+    //     //                 ;
+    //     //             }else {
+    //     //                 break;
+    //     //             }
+    //     //         }else {
+    //     //             break;
+    //     //         }
+    //     //     }else {
+    //     //         break;
+    //     //     }
+    //     //     now += d2;
+    //     // }
 
-        t2 = LOOP(i, n, d1, d2, t2);
-    }
+    //     t2 = LOOP(i, n, d1, d2, t2);
+    // }
+    t2 = STEP4(n-d2, n, d1, d2, t2);
 
     // //STEP5-1
     // for(int i=1; i<n-d1; i++) {
